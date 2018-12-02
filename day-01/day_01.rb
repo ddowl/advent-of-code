78858722c98e3150a3adcf00d49e9d367cc2ee7a
Drew Dowling <drewdowling37@gmail.com>
2018-12-02 13:17:47 -0800

    finished day-01 part 2

--- day-01/day_01.rb
+++ day-01/day_01.rb
@@ -1,1 +1,17 @@
-puts File.readlines('input.txt').map(&:to_i).reduce(:+)
+require 'set'
+
+sum = File.readlines('input.txt').map(&:to_i).reduce(:+)
+puts sum
+
+sums = Set.new()
+running_sum = 0
+# Is there a more efficient way to generate a repeating list on demand?
+seq = File.readlines('input.txt').map(&:to_i) * 100000
+seq.each do |num|
+  running_sum += num
+  if sums.include?(running_sum)
+    puts running_sum
+    break
+  end
+  sums.add running_sum
+end
