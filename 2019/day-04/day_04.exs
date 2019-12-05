98fa756cb6b41247d1bc32b29556f7ddef0c2c7f
Drew Dowling <drewdowling37@gmail.com>
2019-12-04 16:43:39 -0800

    finished day-04 part 2

--- 2019/day-04/day_04.exs
+++ 2019/day-04/day_04.exs
@@ -1,13 +1,17 @@
 defmodule PasswordChecker do
-  def is_valid?(password) do
-    adjacent_digits(password) && non_decreasing_digits(password)
-  end
-
   def adjacent_digits(password) do
     password
     |> String.graphemes()
     |> Enum.chunk_every(2, 1, :discard)
     |> Enum.any?(fn [x, y] -> x == y end)
+  end
+
+  def contains_digit_pair(password) do
+    password
+    |> String.graphemes()
+    |> Enum.chunk_by(& &1)
+    |> Enum.map(&Enum.count/1)
+    |> Enum.member?(2)
   end
 
   def non_decreasing_digits(password) do
@@ -26,8 +30,23 @@ password_range = 156_218..652_527
 num_valid_passwords =
   password_range
   |> Enum.map(&Integer.to_string/1)
-  |> Enum.filter(&PasswordChecker.is_valid?/1)
+  |> Enum.filter(fn password ->
+    PasswordChecker.adjacent_digits(password) && PasswordChecker.non_decreasing_digits(password)
+  end)
   |> Enum.count()
 
-IO.puts("Part 1: Number of valid passwords in range")
+IO.puts("Part 1: Number of valid passwords in range, have adjacent digits, non-decreasing")
+IO.inspect(num_valid_passwords)
+
+# Part 2
+num_valid_passwords =
+  password_range
+  |> Enum.map(&Integer.to_string/1)
+  |> Enum.filter(fn password ->
+    PasswordChecker.contains_digit_pair(password) &&
+      PasswordChecker.non_decreasing_digits(password)
+  end)
+  |> Enum.count()
+
+IO.puts("Part 2: Number of valid passwords in range, have digit pairs, non-decreasing")
 IO.inspect(num_valid_passwords)
