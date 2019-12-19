e40cc2dce3c808bde8411aa948b853e8772f2eb0
Drew Dowling <drewdowling37@gmail.com>
2019-12-18 16:37:24 -0800

    day-13: can draw brickbreaker screen

--- 2019/day-13/day_13.exs
+++ 2019/day-13/day_13.exs
@@ -1,5 +1,27 @@
 Code.require_file("../intcode.ex")
 
+defmodule Arcade do
+  def screen(positions_to_tiles) do
+    positions_to_tiles
+    |> Enum.sort()
+    |> Enum.group_by(fn {{_, y}, _} -> y end)
+    |> Enum.map(fn {_y, x_positions} ->
+      x_positions |> Enum.map(fn {_p, tile_id} -> tile_str(tile_id) end) |> Enum.join()
+    end)
+    |> Enum.join("\n")
+  end
+
+  def tile_str(id) do
+    case id do
+      0 -> " "
+      1 -> "#"
+      2 -> "="
+      3 -> "_"
+      4 -> "o"
+    end
+  end
+end
+
 {:ok, intcode_str} = File.read("input.txt")
 
 arcade_program =
@@ -12,23 +34,27 @@ arcade_program =
 IO.inspect(arcade_program)
 
 # Part 1
-arcade_screen = TtyIntcode.execute(arcade_program) |> Enum.reverse()
+arcade_output = TtyIntcode.execute(arcade_program) |> Enum.reverse()
 
-tiles =
-  arcade_screen
+tiles_to_positions =
+  arcade_output
   |> Enum.chunk_every(3)
   |> List.foldl(%{}, fn [x, y, tile_id], acc ->
-    tile_type =
-      case tile_id do
-        0 -> :empty
-        1 -> :wall
-        2 -> :block
-        3 -> :paddle
-        4 -> :ball
-      end
-
-    positions_with_type = Map.get(acc, tile_type, [])
-    Map.put(acc, tile_type, [{x, y} | positions_with_type])
+    positions_with_type = Map.get(acc, tile_id, [])
+    Map.put(acc, tile_id, [{x, y} | positions_with_type])
   end)
 
-IO.inspect(Map.get(tiles, :block) |> Enum.count())
+IO.inspect(Map.get(tiles_to_positions, 2) |> Enum.count())
+
+# Part 2
+fixed_arcade_program = put_elem(arcade_program, 0, 2)
+
+# verifying output contains all spaces in grid
+positions_to_tiles =
+  arcade_output
+  |> Enum.chunk_every(3)
+  |> List.foldl(%{}, fn [x, y, tile_id], acc ->
+    Map.put(acc, {x, y}, tile_id)
+  end)
+
+IO.puts(Arcade.screen(positions_to_tiles))
