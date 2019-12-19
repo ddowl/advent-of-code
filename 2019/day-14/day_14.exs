c8b8177779f75d71057e6074b7dd389c24aa4a2e
Drew Dowling <drewdowling37@gmail.com>
2019-12-19 09:29:19 -0800

    day-14: try to combine non-ore compounds first, correct up to 3rd example

--- 2019/day-14/day_14.exs
+++ 2019/day-14/day_14.exs
@@ -2,30 +2,56 @@ defmodule Nanofactory do
   def required_ore("ORE", n, _), do: n
 
   def required_ore(product, n, reactions) do
+    compounds = required_compounds(product, n, reactions)
+
+    compounds
+    |> Enum.map(fn {c, m} ->
+      {yield, [{"ORE", o}]} = Map.get(reactions, c)
+      reruns = num_required_reactions(m, yield)
+      reruns * o
+    end)
+    |> Enum.sum()
+  end
+
+  # Aggregate all required non-ORE
+  def required_compounds("ORE", _, _), do: raise("Cannot call 'ORE' on required_compounds")
+
+  def required_compounds(product, n, reactions) do
+    # If product requires only ORE, we're done
     {yield, reactants} = Map.get(reactions, product)
 
-    ore_for_yield_num_products =
-      reactants
-      |> Enum.map(fn {reactant, m} -> required_ore(reactant, m, reactions) end)
-      |> Enum.sum()
+    if only_ore(reactants) do
+      [{product, n}]
+    else
+      reruns = num_required_reactions(n, yield)
 
-    over_n(n, yield) * ore_for_yield_num_products
+      base_compounds =
+        reactants
+        |> Enum.map(fn {r, m} -> required_compounds(r, m * reruns, reactions) end)
+        |> List.flatten()
+        |> Enum.group_by(fn {r, _} -> r end, fn {_, m} -> m end)
+        |> Enum.map(fn {r, ms} -> {r, Enum.sum(ms)} end)
+    end
   end
 
-  def over_n(n, yield) do
-    over_n(n, yield, yield, 1)
+  # TODO: simpler way to specify # of required reactions?
+  defp num_required_reactions(n, yield) do
+    num_required_reactions(n, yield, yield, 1)
   end
 
-  def over_n(n, acc, step, count) do
+  defp num_required_reactions(n, acc, step, count) do
     if acc >= n do
       count
     else
-      over_n(n, acc + step, step, count + 1)
+      num_required_reactions(n, acc + step, step, count + 1)
     end
   end
+
+  defp only_ore([{"ORE", _}]), do: true
+  defp only_ore(_), do: false
 end
 
-{:ok, contents} = File.read("ex1.txt")
+{:ok, contents} = File.read("ex6.txt")
 
 reactions =
   contents
@@ -53,11 +79,9 @@ reactions =
 IO.inspect(reactions)
 
 # Part 1
-IO.inspect({"ORE", Nanofactory.required_ore("ORE", 10, reactions)})
-IO.inspect({"A", Nanofactory.required_ore("A", 10, reactions)})
-IO.inspect({"A", Nanofactory.required_ore("A", 7, reactions)})
-IO.inspect({"B", Nanofactory.required_ore("B", 10, reactions)})
-IO.inspect({"C", Nanofactory.required_ore("C", 1, reactions)})
-IO.inspect({"D", Nanofactory.required_ore("D", 1, reactions)})
-IO.inspect({"E", Nanofactory.required_ore("E", 1, reactions)})
-IO.inspect({"FUEL", Nanofactory.required_ore("FUEL", 1, reactions)})
+# IO.inspect(Nanofactory.required_ore("FUEL", 1, reactions))
+IO.inspect(Nanofactory.required_ore("FUEL", 1, reactions))
+IO.inspect(Nanofactory.required_compounds("FUEL", 1, reactions))
+# IO.inspect(Nanofactory.required_compounds("AB", 2, reactions))
+# IO.inspect(Nanofactory.required_compounds("BC", 3, reactions))
+# IO.inspect(Nanofactory.required_compounds("CA", 4, reactions))
