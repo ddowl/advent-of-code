544f2f1108459d00ec162f94e699676924f90357
Drew Dowling <drewdowling37@gmail.com>
2019-12-20 14:23:33 -0800

    finished day-15 part 2

--- 2019/day-15/day_15.exs
+++ 2019/day-15/day_15.exs
@@ -127,10 +127,10 @@ defmodule Droid do
   defp found_oxygen_tank(state, pos, steps),
     do: %{state | oxygen_tank_pos: pos, oxygen_tank_path_len: steps}
 
-  defp move_in_dir({x, y}, 1), do: {x, y + 1}
-  defp move_in_dir({x, y}, 2), do: {x, y - 1}
-  defp move_in_dir({x, y}, 3), do: {x - 1, y}
-  defp move_in_dir({x, y}, 4), do: {x + 1, y}
+  def move_in_dir({x, y}, 1), do: {x, y + 1}
+  def move_in_dir({x, y}, 2), do: {x, y - 1}
+  def move_in_dir({x, y}, 3), do: {x - 1, y}
+  def move_in_dir({x, y}, 4), do: {x + 1, y}
 
   defp opposite_direction(1), do: 2
   defp opposite_direction(2), do: 1
@@ -166,6 +166,34 @@ defmodule Droid do
   end
 end
 
+defmodule Oxygen do
+  def fill_ship_from_tank(floor_positions, tank_pos) do
+    {_, max_step} = fill_ship(floor_positions, tank_pos, MapSet.new(), 0)
+    max_step
+  end
+
+  # TODO: generalize DFS for Oxygen spread and Droid exploration
+  defp fill_ship(floor_positions, curr_pos, seen, steps_from_origin) do
+    seen = MapSet.put(seen, curr_pos)
+    adjacent_positions = find_adjacent_positions(floor_positions, curr_pos)
+
+    List.foldl(adjacent_positions, {seen, steps_from_origin}, fn p, {seen, steps} ->
+      if !MapSet.member?(seen, p) do
+        {seen_more, other_steps} = fill_ship(floor_positions, p, seen, steps_from_origin + 1)
+        {seen_more, max(steps, other_steps)}
+      else
+        {seen, steps}
+      end
+    end)
+  end
+
+  defp find_adjacent_positions(floor_positions, curr_pos) do
+    [1, 2, 3, 4]
+    |> Enum.map(fn dir -> Droid.move_in_dir(curr_pos, dir) end)
+    |> Enum.filter(fn p -> MapSet.member?(floor_positions, p) end)
+  end
+end
+
 {:ok, intcode_str} = File.read("input.txt")
 
 droid_program =
@@ -181,3 +209,9 @@ IO.inspect(droid_program)
 ship_floorplan = Droid.init(droid_program)
 IO.inspect(ship_floorplan)
 IO.inspect(ship_floorplan.oxygen_tank_path_len)
+
+# Part 2
+# DFS max depth on floor graph, starting at oxygen tank
+floors = ship_floorplan.floors
+tank_pos = ship_floorplan.oxygen_tank_pos
+IO.inspect(Oxygen.fill_ship_from_tank(floors, tank_pos))
