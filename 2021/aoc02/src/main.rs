7eab544f1cdf2f9147af1b9ad44c544ab84aa371
Drew Dowling <drewdowling37@gmail.com>
2021-12-02 01:01:30 -0800

    day 2, use enum for instruction directions

--- 2021/aoc02/src/main.rs
+++ 2021/aoc02/src/main.rs
@@ -1,6 +1,13 @@
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 
+#[derive(Eq, PartialEq)]
+enum Direction {
+    Forward,
+    Up,
+    Down,
+}
+
 fn main() {
     let filename = "input/test.txt";
     // Open the file in read-only mode (ignoring errors).
@@ -8,40 +15,48 @@ fn main() {
     let reader = BufReader::new(file);
 
     // Read the file line by line using the lines() iterator from std::io::BufRead.
-    let instructions: Vec<(String, i32)> = reader
+    // TODO: Can I propagate validation errors as a `Result<Vec<Direction, i32>>`?
+    let instructions: Vec<(Direction, i32)> = reader
         .lines()
         .enumerate()
         .map(|(_idx, line_res)| {
             let line = line_res.expect("couldn't read line!");
             let tokens: Vec<&str> = line.split(" ").collect();
-            (tokens[0].to_string(), tokens[1].parse().unwrap())
+            let direction: Direction = match tokens[0] {
+                "forward" => Direction::Forward,
+                "up" => Direction::Up,
+                "down" => Direction::Down,
+                x => panic!("invalid direction: {}", x),
+            };
+            (direction, tokens[1].parse().unwrap())
         })
         .collect();
 
     // println!("{:?}", instructions);
 
-    let horizontal_distance = count_horizontal(&instructions);
+    // Part 1
+    let distance = count_distance(&instructions);
     let depth = count_depth(&instructions);
-    println!("horizontal {}", horizontal_distance);
+    println!("distance {}", distance);
     println!("depth {}", depth);
-    println!("product {}", horizontal_distance * depth);
+    println!("product {}", distance * depth);
 }
 
-fn count_horizontal(instructions: &Vec<(String, i32)>) -> i32 {
+fn count_distance(instructions: &Vec<(Direction, i32)>) -> i32 {
     instructions
         .iter()
-        .filter(|(dir, _)| dir == "forward")
+        .filter(|(dir, _)| dir == &Direction::Forward)
         .map(|(_, mag)| mag)
         .sum()
 }
 
-fn count_depth(instructions: &Vec<(String, i32)>) -> i32 {
+fn count_depth(instructions: &Vec<(Direction, i32)>) -> i32 {
     instructions
         .iter()
-        .map(|(dir, mag)| match dir.as_str() {
-            "down" => *mag,
-            "up" => -mag,
-            _ => 0,
+        .map(|(dir, mag)| match dir {
+            Direction::Forward => 0,
+            Direction::Up => -mag,
+            Direction::Down => *mag,
         })
         .sum()
 }
