dcf2526d555913fdd69d5334c1674c6a349bab2f
Drew Dowling <drewdowling37@gmail.com>
2021-12-02 11:10:47 -0800

    day 2, parse input fn

--- 2021/aoc02/src/main.rs
+++ 2021/aoc02/src/main.rs
@@ -1,3 +1,4 @@
+use std::error::Error;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 
@@ -8,44 +9,57 @@ enum Direction {
     Down,
 }
 
+type ParseResult<T> = Result<T, Box<dyn Error>>;
+
 fn main() {
     let filename = "input/input.txt";
+    let result: ParseResult<Vec<(Direction, i32)>> = parse_input_file(filename);
+    match result {
+        Err(e) => {
+            println!("Error parsing input file: {}", e);
+        }
+
+        Ok(instructions) => {
+            // println!("{:?}", instructions);
+
+            // Part 1
+            let distance = count_distance(&instructions);
+            let depth = count_depth(&instructions);
+            println!("distance {}", distance);
+            println!("depth {}", depth);
+            println!("product {}", distance * depth);
+
+            // Part 2
+            let (distance, depth) = count_with_aim(&instructions);
+            println!("distance {}", distance);
+            println!("depth {}", depth);
+            println!("product {}", distance * depth);
+        }
+    }
+}
+
+fn parse_input_file(filename: &str) -> ParseResult<Vec<(Direction, i32)>> {
     // Open the file in read-only mode (ignoring errors).
-    let file = File::open(filename).unwrap();
+    let file = File::open(filename)?;
     let reader = BufReader::new(file);
 
     // Read the file line by line using the lines() iterator from std::io::BufRead.
-    // TODO: Can I propagate validation errors as a `Result<Vec<Direction, i32>>`?
-    let instructions: Vec<(Direction, i32)> = reader
+    reader
         .lines()
         .enumerate()
-        .map(|(_idx, line_res)| {
-            let line = line_res.expect("couldn't read line!");
+        .map(|(_, line_res)| {
+            let line = line_res?; // `line` needs to live till end of the closure
             let tokens: Vec<&str> = line.split(" ").collect();
             let direction: Direction = match tokens[0] {
                 "forward" => Direction::Forward,
                 "up" => Direction::Up,
                 "down" => Direction::Down,
-                x => panic!("invalid direction: {}", x),
+                x => return Err(format!("invalid direction: {}", x).into()),
             };
-            (direction, tokens[1].parse().unwrap())
+            let magnitude = tokens[1].parse()?;
+            Ok((direction, magnitude))
         })
-        .collect();
-
-    // println!("{:?}", instructions);
-
-    // Part 1
-    let distance = count_distance(&instructions);
-    let depth = count_depth(&instructions);
-    println!("distance {}", distance);
-    println!("depth {}", depth);
-    println!("product {}", distance * depth);
-
-    // Part 2
-    let (distance, depth) = count_with_aim(&instructions);
-    println!("distance {}", distance);
-    println!("depth {}", depth);
-    println!("product {}", distance * depth);
+        .collect()
 }
 
 fn count_distance(instructions: &Vec<(Direction, i32)>) -> i32 {
