f0e5437ac298b48b2cb1b42be6e083c97f0b3336
Drew Dowling <drewdowling37@gmail.com>
2021-12-03 10:05:50 -0800

    day 3, refactor part 2 with BitCriteria

--- 2021/aoc03/src/main.rs
+++ 2021/aoc03/src/main.rs
@@ -18,7 +18,9 @@ fn main() {
     println!("epsilon rate: {}", epsilon_str);
     println!("epsilon rate: {}", epsilon_val);
 
-    println!("gamma * epsilon: {}", gamma_val * epsilon_val);
+    let power_consumption = gamma_val * epsilon_val;
+    println!("power consumption: {}", power_consumption);
+    assert_eq!(3882564, power_consumption);
 
     // Part 2
     let oxygen_generator_rating = calculate_oxygen_generator_rating(&binary_numbers);
@@ -27,10 +29,9 @@ fn main() {
     let co2_scrubber_rating = calculate_co2_scrubber_rating(&binary_numbers);
     println!("co2_scrubber_rating: {}", co2_scrubber_rating);
 
-    println!(
-        "oxygen * co2: {}",
-        oxygen_generator_rating * co2_scrubber_rating
-    );
+    let life_support_rating = oxygen_generator_rating * co2_scrubber_rating;
+    println!("life support rating: {}", life_support_rating);
+    assert_eq!(3385170, life_support_rating);
 }
 
 fn parse_input_file(filename: &str) -> Vec<String> {
@@ -50,83 +51,49 @@ fn calculate_gamma_rate_str(binary_numbers: &Vec<String>) -> String {
         binary_numbers.iter().map(|n| n.chars().collect()).collect();
 
     (0..binary_numbers[0].len())
-        .map(|i| {
-            let (num_zeros, num_ones) = count_digits(&binary_nums_as_chars, i);
-            if num_zeros > num_ones {
-                '0'
-            } else {
-                '1'
-            }
-        })
+        .map(|i| most_common_bit(&binary_nums_as_chars, i))
         .collect::<String>()
 }
 
-fn binary_complement(binary_str: &str) -> String {
-    String::from(binary_str)
-        .chars()
-        .map(|d| if d == '0' { '1' } else { '0' })
-        .collect()
-}
-
-fn binary_str_to_num(binary_str: &str) -> isize {
-    isize::from_str_radix(binary_str, 2).unwrap()
-}
-
 fn calculate_oxygen_generator_rating(binary_numbers: &Vec<String>) -> isize {
-    let mut binary_nums_as_chars: Vec<Vec<char>> =
-        binary_numbers.iter().map(|n| n.chars().collect()).collect();
-
-    let mut bit_position = 0;
-    while binary_nums_as_chars.len() != 1 {
-        let common_bit = most_common_bit(&binary_nums_as_chars, bit_position);
-
-        // filter binary_numbers by only those that have the most common bit in this position
-        let filtered_nums: Vec<Vec<char>> = binary_nums_as_chars
-            .into_iter()
-            .filter(|bn| bn[bit_position] == common_bit)
-            .collect();
-
-        binary_nums_as_chars = filtered_nums;
-        bit_position += 1;
-    }
-
-    let oxygen_rating_str: String = binary_nums_as_chars
-        .first()
-        .expect("expected to find last binary number")
-        .iter()
-        .collect();
-
-    println!("oxygen_rating_str: {}", oxygen_rating_str);
-    binary_str_to_num(&oxygen_rating_str)
+    filter_binary_nums_by_criteria(binary_numbers, most_common_bit)
 }
 
 fn calculate_co2_scrubber_rating(binary_numbers: &Vec<String>) -> isize {
+    filter_binary_nums_by_criteria(binary_numbers, least_common_bit)
+}
+
+fn filter_binary_nums_by_criteria(
+    binary_numbers: &Vec<String>,
+    bit_criteria: BitCriteria,
+) -> isize {
     let mut binary_nums_as_chars: Vec<Vec<char>> =
         binary_numbers.iter().map(|n| n.chars().collect()).collect();
 
     let mut bit_position = 0;
     while binary_nums_as_chars.len() != 1 {
-        let common_bit = least_common_bit(&binary_nums_as_chars, bit_position);
+        let column_bit_value = bit_criteria(&binary_nums_as_chars, bit_position);
 
         // filter binary_numbers by only those that have the most common bit in this position
         let filtered_nums: Vec<Vec<char>> = binary_nums_as_chars
             .into_iter()
-            .filter(|bn| bn[bit_position] == common_bit)
+            .filter(|bn| bn[bit_position] == column_bit_value)
             .collect();
 
         binary_nums_as_chars = filtered_nums;
         bit_position += 1;
     }
 
-    let co2_scrubber_rating_str: String = binary_nums_as_chars
+    let rating_str: String = binary_nums_as_chars
         .first()
         .expect("expected to find last binary number")
         .iter()
         .collect();
 
-    println!("oxygen_rating_str: {}", co2_scrubber_rating_str);
-    binary_str_to_num(&co2_scrubber_rating_str)
+    binary_str_to_num(&rating_str)
 }
+
+type BitCriteria = fn(binary_nums_as_chars: &Vec<Vec<char>>, bit_position: usize) -> char;
 
 fn most_common_bit(binary_nums_as_chars: &Vec<Vec<char>>, i: usize) -> char {
     let (num_zeros, num_ones) = count_digits(binary_nums_as_chars, i);
@@ -139,11 +106,7 @@ fn most_common_bit(binary_nums_as_chars: &Vec<Vec<char>>, i: usize) -> char {
 
 fn least_common_bit(binary_nums_as_chars: &Vec<Vec<char>>, i: usize) -> char {
     let most_common_bit = most_common_bit(binary_nums_as_chars, i);
-    if most_common_bit == '0' {
-        '1'
-    } else {
-        '0'
-    }
+    flip(most_common_bit)
 }
 
 fn count_digits(binary_nums_as_chars: &Vec<Vec<char>>, i: usize) -> (usize, usize) {
@@ -157,4 +120,20 @@ fn count_digits(binary_nums_as_chars: &Vec<Vec<char>>, i: usize) -> (usize, usiz
                 (num_zeros, num_ones + 1)
             }
         })
+}
+
+fn binary_str_to_num(binary_str: &str) -> isize {
+    isize::from_str_radix(binary_str, 2).unwrap()
+}
+
+fn binary_complement(binary_str: &str) -> String {
+    String::from(binary_str).chars().map(flip).collect()
+}
+
+fn flip(bit: char) -> char {
+    if bit == '0' {
+        '1'
+    } else {
+        '0'
+    }
 }
