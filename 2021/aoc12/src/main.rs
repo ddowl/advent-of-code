6fc15a227f90225b6393f381992752b51746c152
Drew Dowling <drewdowling37@gmail.com>
2021-12-11 22:51:27 -0800

    day 12, part 1

---
+++ 2021/aoc12/src/main.rs
@@ -0,0 +1,101 @@
+use std::collections::{HashMap, HashSet};
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+
+#[derive(Debug)]
+struct CaveNetwork {
+    adj_list: HashMap<String, Vec<String>>,
+}
+
+impl CaveNetwork {
+    fn new(edges: Vec<(String, String)>) -> CaveNetwork {
+        let mut adj_list: HashMap<String, Vec<String>> = HashMap::new();
+
+        for (a, b) in edges {
+            let traversable_from_a = adj_list.entry(a.clone()).or_default();
+            traversable_from_a.push(b.clone());
+            let traversable_from_b = adj_list.entry(b).or_default();
+            traversable_from_b.push(a);
+        }
+
+        CaveNetwork { adj_list }
+    }
+
+    fn explore_all_paths(&self) -> Vec<Vec<String>> {
+        let mut paths_to_end = vec![];
+        self.explore_all_paths_helper(
+            "start".to_string(),
+            vec![],
+            HashSet::new(),
+            &mut paths_to_end,
+        );
+        paths_to_end
+    }
+
+    fn explore_all_paths_helper(
+        &self,
+        curr_cave: String,
+        mut curr_path: Vec<String>,
+        mut visited_small_caves: HashSet<String>,
+        paths_to_end: &mut Vec<Vec<String>>,
+    ) {
+        // println!("curr_path: {:?}, curr_cave: {:?}", curr_path, curr_cave);
+        curr_path.push(curr_cave.clone());
+        if curr_cave == "end" {
+            paths_to_end.push(curr_path);
+        } else {
+            // not the end KEEP SEARCHING
+            if !visited_small_caves.contains(&curr_cave) {
+                if !curr_cave.chars().next().unwrap().is_uppercase() {
+                    // small caves can only be searched once
+                    visited_small_caves.insert(curr_cave.clone());
+                }
+
+                // copy current path and branch out from here
+                let maybe_neighbor_caves = self.adj_list.get(&curr_cave);
+                if let Some(neighbor_caves) = maybe_neighbor_caves {
+                    for neighbor in neighbor_caves {
+                        self.explore_all_paths_helper(
+                            neighbor.clone(),
+                            curr_path.clone(),
+                            visited_small_caves.clone(),
+                            paths_to_end,
+                        );
+                    }
+                }
+            }
+        }
+    }
+}
+
+fn main() {
+    let filename = "input/input.txt";
+    let cave: CaveNetwork = parse_input_file(filename);
+
+    println!("cave: {:?}", cave);
+    println!();
+
+    let paths = cave.explore_all_paths();
+    // println!("all paths through cave:");
+    // println!("{:?}", paths);
+    println!("num paths through cave: {}", paths.len());
+}
+
+fn parse_input_file(filename: &str) -> CaveNetwork {
+    // Open the file in read-only mode (ignoring errors).
+    let file = File::open(filename).expect("couldn't open file");
+    let reader = BufReader::new(file);
+
+    // Read the file line by line using the lines() iterator from std::io::BufRead.
+    let edges: Vec<(String, String)> = reader
+        .lines()
+        .map(|l| {
+            let line = l.unwrap();
+            let parts: Vec<&str> = line.split('-').collect();
+            assert_eq!(parts.len(), 2);
+            (parts[0].to_string(), parts[1].to_string())
+        })
+        .collect();
+
+    CaveNetwork::new(edges)
+}
