8eb57cf2b94b5a347605f75b7677722207894272
Drew Dowling <drewdowling37@gmail.com>
2023-12-08 14:42:00 -0800

    setup day 1 pt 2

--- 2023/day-01/pt1/trebuchet.go
+++ 2023/day-01/pt1/trebuchet.go
@@ -10,34 +10,42 @@ import (
 
 var DigitRegex = regexp.MustCompile("[0-9]")
 
-func readFile(filename string) []string {
+func readFile(filename string) ([]string, error) {
 	content, err := os.ReadFile(filename) // the file is inside the local directory
 	if err != nil {
-		fmt.Println("Err")
+		return nil, err
 	}
-	return strings.Split(string(content), "\n")
+	return strings.Split(string(content), "\n"), nil
 }
 
-func calibrationValue(s string) int {
+func calibrationValue(s string) (int, error) {
 	digits := DigitRegex.FindAllString(s, -1)
 	if len(digits) == 0 {
-		return 0
+		return 0, nil
 	}
 	firstLast := fmt.Sprintf("%s%s", digits[0], digits[len(digits)-1])
-	fmt.Println(digits)
 	cv, err := strconv.Atoi(firstLast)
 	if err != nil {
-		panic(err)
-		// fmt.Println(err)
+		return -1, err
 	}
-	return cv
+	return cv, nil
 }
 
 func main() {
-	lines := readFile("input.txt")
+	lines, err := readFile("input.txt")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
 	calibrationValueSum := 0
 	for _, line := range lines {
-		calibrationValueSum += calibrationValue(line)
+		cv, err := calibrationValue(line)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		calibrationValueSum += cv
 	}
 	fmt.Println("calibrationValueSum: ", calibrationValueSum)
 }
