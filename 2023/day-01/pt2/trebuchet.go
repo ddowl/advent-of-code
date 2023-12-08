2de11cea548d2cc076c2cb6831752b42e154530b
Drew Dowling <drewdowling37@gmail.com>
2023-12-08 15:13:14 -0800

    finished day 1 pt 2

--- 2023/day-01/pt2/trebuchet.go
+++ 2023/day-01/pt2/trebuchet.go
@@ -6,15 +6,44 @@ import (
 	"unicode"
 )
 
+var SpelledDigits = map[string]int{
+	"one":   1,
+	"two":   2,
+	"three": 3,
+	"four":  4,
+	"five":  5,
+	"six":   6,
+	"seven": 7,
+	"eight": 8,
+	"nine":  9,
+}
+
+func spelledDigit(s string, idx int) *int {
+	for key, value := range SpelledDigits {
+		if len(s)-idx >= len(key) {
+			slice := s[idx:(idx + len(key))]
+			if slice == key {
+				return &value
+			}
+		}
+	}
+	return nil
+}
+
 func calibrationValue(s string) int {
 	firstDigit := 0
 	lastDigit := 0
-	for _, c := range s {
+	for i, c := range s {
 		if unicode.IsDigit(c) {
 			if firstDigit == 0 {
 				firstDigit = int(c - '0')
 			}
 			lastDigit = int(c - '0')
+		} else if digit := spelledDigit(s, i); digit != nil {
+			if firstDigit == 0 {
+				firstDigit = *digit
+			}
+			lastDigit = *digit
 		}
 	}
 	return 10*firstDigit + lastDigit
