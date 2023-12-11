4b8b3f3d0d1651f4babced1f930fa97c5103e470
Drew Dowling <drewdowling37@gmail.com>
2023-12-10 17:07:34 -0800

    day 5 part 1

--- 2023/day-05/pt1/farming.go
+++ 2023/day-05/pt1/farming.go
@@ -3,12 +3,41 @@ package main
 import (
 	"aoc/day-05/input"
 	"fmt"
+
+	"github.com/samber/lo"
 )
 
+type CategoryMap struct {
+	Ranges []input.Range
+}
+
+func (cm CategoryMap) ToDestCategory(n int) int {
+	for _, rg := range cm.Ranges {
+		if n >= rg.SourceStart && n <= (rg.SourceStart+rg.Len) {
+			// `n` falls into this range!
+			return rg.DestStart + n - rg.SourceStart
+		}
+	}
+	return n
+}
+
 func main() {
-	almanac, err := input.ParseTest()
+	almanac, err := input.Parse()
 	if err != nil {
 		panic(err)
 	}
-	fmt.Printf("%+v\n", almanac)
+	// fmt.Printf("%+v\n", almanac)
+
+	categoryMaps := lo.Map(almanac.Maps, func(rgs []input.Range, _ int) CategoryMap {
+		return CategoryMap{Ranges: rgs}
+	})
+
+	locationNums := lo.Map(almanac.Seeds, func(seed int, _ int) int {
+		catNum := seed
+		for _, cm := range categoryMaps {
+			catNum = cm.ToDestCategory(catNum)
+		}
+		return catNum
+	})
+	fmt.Println(lo.Min(locationNums))
 }
