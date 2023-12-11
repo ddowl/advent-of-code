d1f94ae7fa860c96d6e361a78f3694cb18a7d5cd
Drew Dowling <drew.dowling@verkada.com>
2023-12-10 20:28:49 -0800

    day 6 pt 1

--- 2023/day-06/pt1/wait_for_it.go
+++ 2023/day-06/pt1/wait_for_it.go
@@ -2,13 +2,44 @@ package main
 
 import (
 	"aoc/day-06/input"
+	"errors"
 	"fmt"
+
+	"github.com/samber/lo"
 )
 
+func numWaysToWin(race input.Race) (int, error) {
+	start := -1
+	fmt.Println("processing race: ", race)
+	for t := 0; t <= race.Time; t++ {
+		v := t
+		distance := v * (race.Time - t)
+		// fmt.Println("distance: ", distance)
+		if distance > race.Distance && start == -1 {
+			start = t
+		}
+		if distance <= race.Distance && start != -1 {
+			println("found a way to win: ", start, t)
+			return t - start, nil
+		}
+	}
+	return -1, errors.New("no way to win")
+}
+
 func main() {
-	races, err := input.ParseTest()
+	races, err := input.Parse()
 	if err != nil {
 		panic(err)
 	}
 	fmt.Printf("%+v\n", races)
+	winCounts := lo.Map(races, func(race input.Race, _ int) int {
+		waysToWin, err := numWaysToWin(race)
+		if err != nil {
+			panic(err)
+		}
+		return waysToWin
+	})
+	fmt.Println("ways to win each race: ", winCounts)
+	product := lo.Reduce(winCounts, func(acc int, wins int, _ int) int { return acc * wins }, 1)
+	fmt.Println("total product: ", product)
 }
