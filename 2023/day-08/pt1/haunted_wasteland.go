71e0f184ad5a988ebf871415736b41e151597a60
Drew Dowling <drew.dowling@verkada.com>
2023-12-10 22:19:04 -0800

    day 8 pt 1

--- 2023/day-08/pt1/haunted_wasteland.go
+++ 2023/day-08/pt1/haunted_wasteland.go
@@ -3,9 +3,27 @@ package main
 import (
 	"aoc/day-08/input"
 	"fmt"
+
+	"github.com/samber/lo"
 )
 
 func main() {
-	network := input.Parse("input/test.txt")
-	fmt.Printf("%+v\n", network)
+	network := input.Parse("input/input.txt")
+	// fmt.Printf("%+v\n", network)
+
+	router := map[string][]string{}
+	for _, node := range network.Nodes {
+		router[node.Src] = []string{node.Left, node.Right}
+	}
+	// fmt.Printf("%+v\n", router)
+
+	currNode := "AAA"
+	for steps := 0; true; steps++ {
+		if currNode == "ZZZ" {
+			fmt.Printf("Found ZZZ after %d steps\n", steps)
+			break
+		}
+		instruction := string(network.Instructions[steps%len(network.Instructions)])
+		currNode = router[currNode][lo.Ternary(instruction == "L", 0, 1)]
+	}
 }
